850a8e6c01b5e56730d9e10ef5703ec6f3aa7251
angelherreraP <a.manuel.herrera.p@gmail.com>
2026-02-14 08:08:32 -0600

    Allow all CORS origins

--- aws_final/src/main/java/com/example/aws_final/shared/config/WebConfig.java
+++ aws_final/src/main/java/com/example/aws_final/shared/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permitir todas las rutas
-                .allowedOrigins("http://localhost:4200") // El puerto de tu Angular
+                .allowedOrigins("*") // Allow all for now (or inject from env)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false); // Must be false when using *
     }
 }
